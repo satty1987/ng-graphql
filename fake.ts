7d5c297dc4f410f56c2d1715722a2b979736ee44
satty1987 <satnammca@gmail.com>
2019-09-07 17:19:41 +0530

    Update fake.ts

--- fake.ts
+++ fake.ts
@@ -1,33 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '../../core/services/okta-auth.service';
+import { ActivatedRoute } from '@angular/router';
+import { API_URLS } from '../../core/constants/api-urls';
 import { NewsService } from 'src/app/core/services/news.service';
 import * as _ from 'lodash';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { AcceptComponent } from '../accept/accept.component';
+
+
 @Component({
-  selector: 'app-seat-availability',
-  templateUrl: './seat-availability.component.html',
-  styleUrls: ['./seat-availability.component.scss']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
-export class SeatAvailabilityComponent implements OnInit {
-  dataList = [];
-  totalParking = 0;
-  allocateParking = 0;
-  availableParking = 0;
-  constructor(public apiService: NewsService) { }
- 
+export class HeaderComponent implements OnInit {
+  showDropdown = false;
+  category = API_URLS.category;
+  value;
+  enableNavigation = false;
+  constructor(public _OktaAuthService: OktaAuthService, public router: ActivatedRoute, 
+    public dialog: MatDialog,
+    public appService: NewsService) { }
 
   ngOnInit() {
-    const path = '';
-    this.apiService.getHttpRequest(path).subscribe((data) => {
-      this.transformData(data);
+    this.appService.notify.subscribe((response) => {
+      this.enableNavigation = response;
     });
+    if (!_.isNull(sessionStorage.getItem('userInfo'))) {
+      this.enableNavigation = true;
+    }
   }
-  transformData(data) {
-    this.totalParking = _.sumBy(data, (item: any) => {
-      return item.totalParking;
-    });
-    this.allocateParking = _.sumBy(data, (item: any) => {
-      return item.AllocatedParking;
-    });
-    this.availableParking = this.totalParking - this.allocateParking;
-
+  toggle() {
+    this.showDropdown = !this.showDropdown;
   }
+  openDialog(): void {
+    this.dialog.open(AcceptComponent,
+       {
+         width: '400px',
+         data: sessionStorage.getItem('userInfo')}
+    );
+   }
 }
